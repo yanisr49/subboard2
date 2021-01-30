a4c82fde47e7a7ef1fea2de9bd953ac72ac65fa1
yanisr49 <yanisrichard@live.fr>
2021-01-30 18:51:53 +0100

    first commit

---
+++ src/main/java/com/pftc/subboard/SubboardApplication.java
@@ -0,0 +1,13 @@
+package com.pftc.subboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pftc/subboard/SubboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pftc.subboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
